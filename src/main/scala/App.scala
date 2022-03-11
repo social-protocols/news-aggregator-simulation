adfa85d7542a6a0f85f23d33f6216f8462e6e717
Felix Dietze <felix.dietze@rwth-aachen.de>
2022-03-11 17:26:18 -0600

    Split up Main into App and LoadCss

---
+++ src/main/scala/App.scala
@@ -0,0 +1,82 @@
+package simulation
+
+import outwatch._
+import outwatch.dsl._
+import colibri._
+import cats.effect.SyncIO
+
+object App {
+  val app = {
+    val liveNewPage     = Subject.behavior[Seq[Submission]](Nil)
+    val liveTopPage     = Subject.behavior[Seq[Submission]](Nil)
+    val bestQualityPage = Subject.behavior[Seq[Submission]](Nil)
+    val tickTime        = Subject.behavior(1000)
+    val resetTrigger    = Subject.behavior(())
+    val subSteps        = 600
+    val tick            = resetTrigger
+      .combineLatest(tickTime)
+      .switchMap { case (_, tickTime) =>
+        Observable
+          .intervalMillis(tickTime)
+          .map(_ => if (tickTime == 0) subSteps else 1)
+      }
+      .publish
+
+    tick.value.foreach { substeps =>
+      for (_ <- 0 until substeps)
+        Simulation.nextStep()
+    }
+
+    // visualization runs independently of simulation
+    tick.value.sampleMillis(33).foreach { _ =>
+      liveNewPage.onNext(Simulation.newpage(Simulation.submissions).toSeq)
+      liveTopPage.onNext(Simulation.frontpage(Simulation.submissions).toSeq)
+      bestQualityPage.onNext(Simulation.bestQualityFrontpage(Simulation.submissions).toSeq)
+    }
+
+    div(
+      tick.value.scan(0L)((sum, substeps) => sum + substeps).map(timeSpanFromSeconds),
+      managed(SyncIO(tick.connect())),
+      SpeedSlider(tickTime),
+      div(
+        display.flex,
+        liveNewPage.map(x => showPage("New", x)),
+        liveTopPage.map(x => showPage("Frontpage", x)),
+      ),
+    )
+  }
+
+  def showPage(title: String, submissions: Seq[Submission]) =
+    div(
+      cls := "p-5",
+      div(title),
+      submissions.take(30).map(showSubmission),
+    )
+
+  def timeSpanFromSeconds(seconds: Long): String = {
+    val ageHours = seconds / 3600
+    val ageMin   = (seconds % 3600) / 60
+    s"${if (ageHours == 0) s"${ageMin}min" else s"${ageHours}h"}"
+  }
+
+  def showSubmission(submission: Submission): HtmlVNode = {
+    val title    = s"Story ${submission.id}"
+    val subtitle =
+      s"${submission.score} points, ${timeSpanFromSeconds(Simulation.timeSeconds - submission.timeSeconds)} ago"
+
+    val qualityAlpha = Math.min(submission.quality / 0.04, 1.0)
+    div(
+      cls := "mt-2",
+      div(
+        div(
+          cls     := "bg-blue-400 inline-block mr-1 rounded-sm",
+          opacity := qualityAlpha,
+          width   := "10px",
+          height  := "10px",
+        ),
+        title,
+      ),
+      div(subtitle, opacity := 0.5),
+    )
+  }
+}
